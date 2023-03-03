c1d7c8b8b3f09ee191999bf7e638522661a0f897
Ranii07 <philnkhata@gmail.com>
2023-03-03 08:43:48 +0200

    new changes  to learning with c string dir

---
+++ arrays_/loop_through_an_array.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int myNumbers[] = {123,167,198,200,45,79,90,70};
+    int i;
+
+    for (i = 0; i <= 4; i++) 
+    {
+        printf("%d\n", myNumbers[i]);
+    }
+    return 0;
+}
