e1bdebc2427b310ee27df75534ae924e0dd1f57f
Ranii07 <philnkhata@gmail.com>
2023-03-03 08:45:01 +0200

    commit changes loop_through_string

---
+++ strings_/loop_through_string.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char carName[] = "Toyota Corolla";
+    int i;
+
+    for (i = 0; i <= 15; i++)
+    {
+        printf("%c\n", carName[i]);
+    }
+    return 0;
+}
